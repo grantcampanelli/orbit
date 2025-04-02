464bb3d2a2cafcb8e0334ca04aa29a900f0927f5
Grant Campanelli <grantcampanelli@gmail.com>
2025-04-01 19:49:00 -0700

    Can deploy

--- prisma/migrations/20240227143405_fix_shelf_item/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `shelfItemId` on the `Bottle` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Bottle" DROP COLUMN "shelfItemId";
