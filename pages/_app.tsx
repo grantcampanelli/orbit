634bd6c4defafd75e6a680538f1c1f6ec433736d
Grant Campanelli <grantcampanelli@gmail.com>
2025-04-02 21:11:07 -0700

    Fixing up pnpm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,17 @@ import Header from "../components/header";
 import "@mantine/core/styles.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider } from "next-auth/react";
-import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { MantineProvider, ColorSchemeScript, AppShell, Skeleton, Group, Burger } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
+import { useDisclosure } from '@mantine/hooks';
+import Image from "next/image";
+import React from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+    const [opened, { toggle }] = useDisclosure();
   return (
     <>
       <Head>
@@ -30,8 +34,36 @@ export default function App({
             }}
         >
           <ModalsProvider>
-            <Header />
-            <Component {...pageProps} />
+              <AppShell
+                  header={{ height: 120 }}
+                  navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+                  padding="md"
+              >
+                  <AppShell.Header>
+                      <Group h="100%" px="md">
+                          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                          <Image
+                              src="/orbit.png"
+                              width={120}
+                              height={100}
+                              alt="Orbit Logo"
+                          />
+                      </Group>
+                  </AppShell.Header>
+                  <AppShell.Navbar p="md">
+                      Navbar
+                      {Array(15)
+                          .fill(0)
+                          .map((_, index) => (
+                              <Skeleton key={index} h={28} mt="sm" animate={false} />
+                          ))}
+                  </AppShell.Navbar>
+                  <AppShell.Main><Component {...pageProps} /></AppShell.Main>
+
+              </AppShell>
+
+
+
           </ModalsProvider>
         </MantineProvider>
       </SessionProvider>
